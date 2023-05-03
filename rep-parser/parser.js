574b32479224951d52c8f2dc01b5f923ff70ac6f
Adrian Ayala <adrian.ayala17@gmail.com>
2023-05-03 12:44:15 -0600

    Mejora el script de los REPs

--- rep-parser/parser.js
+++ rep-parser/parser.js
@@ -3,71 +3,80 @@ import parser from 'xml2json';
 import templateFile from 'template-file';
 
 async function ls(path) {
-  const files = await fs.promises.readdir(path);
+  const folders = await fs.promises.readdir(path);
   const { renderToFolder } = templateFile;
 
-  for (const file of files) {
-    if (file !== '.DS_Store' && file !== '.gitkeep') {
-      fs.readFile(`${path}/${file}`, async function (err, data) {
-        const XML = JSON.parse(parser.toJson(data));
+  for (const folder of folders) {
+    if (folder !== '.DS_Store' && folder !== '.gitkeep') {
+      fs.readdir(`${path}/${folder}`, async function (err, file) {
+        let xmlPath;
 
-        const folder =
-          XML['cfdi:Comprobante']['Serie'] + XML['cfdi:Comprobante']['Folio'];
+        if (file.length > 1) {
+          xmlPath = `${path}/${folder}/${file[1]}`;
+        } else {
+          xmlPath = `${path}/${folder}/${file[0]}`;
+        }
+        fs.readFile(xmlPath, async function (err, data) {
+          const XML = JSON.parse(parser.toJson(data));
 
-        const concepto =
-          XML['cfdi:Comprobante']['cfdi:Conceptos']['cfdi:Concepto'];
+          const folder =
+            XML['cfdi:Comprobante']['Serie'] + XML['cfdi:Comprobante']['Folio'];
 
-        const values = {
-          RFC: XML['cfdi:Comprobante']['cfdi:Receptor']['Rfc'],
-          idCatFormaPago:
-            XML['cfdi:Comprobante']['MetodoPago'] === 'PUE' ? 1 : 2,
-          serie: XML['cfdi:Comprobante']['Serie'],
-          folio: XML['cfdi:Comprobante']['Folio'],
-          nombre: XML['cfdi:Comprobante']['cfdi:Receptor']['Nombre'],
-          uuid: XML['cfdi:Comprobante']['cfdi:Complemento'][
-            'tfd:TimbreFiscalDigital'
-          ]['UUID'],
-          fecha: XML['cfdi:Comprobante']['Fecha'].replace('T', ' '),
-          version: XML['cfdi:Comprobante']['Version'],
-          relacionados: `,null,'${XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos']['pago20:Pago']['pago20:DoctoRelacionado']['IdDocumento']}'`,
-          uuidRelacionado:
-            XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
-              'pago20:Pago'
-            ]['pago20:DoctoRelacionado']['IdDocumento'],
-          claveProdServ: concepto['ClaveProdServ'],
-          descripcion: concepto['Descripcion'],
-          numParcialidad:
-            XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
-              'pago20:Pago'
-            ]['pago20:DoctoRelacionado']['NumParcialidad'],
-          impSaldoAnt:
-            XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
-              'pago20:Pago'
-            ]['pago20:DoctoRelacionado']['ImpSaldoAnt'],
-          impSaldoInsoluto:
-            XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
-              'pago20:Pago'
-            ]['pago20:DoctoRelacionado']['ImpSaldoInsoluto'],
-          impPagado:
-            XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
-              'pago20:Pago'
-            ]['pago20:DoctoRelacionado']['ImpPagado'],
-          fechaPago: XML['cfdi:Comprobante']['cfdi:Complemento'][
-            'pago20:Pagos'
-          ]['pago20:Pago']['FechaPago'].replace('T', ' '),
-          fechaCreacion: XML['cfdi:Comprobante']['cfdi:Complemento'][
-            'tfd:TimbreFiscalDigital'
-          ]['FechaTimbrado'].replace('T', ' '),
-          fechaQuery: XML['cfdi:Comprobante']['cfdi:Complemento'][
-            'pago20:Pagos'
-          ]['pago20:Pago']['FechaPago'].substring(0, 10),
-        };
+          const concepto =
+            XML['cfdi:Comprobante']['cfdi:Conceptos']['cfdi:Concepto'];
 
-        await renderToFolder(
-          './rep-parser/queries.sql',
-          `./queries/${folder}/`,
-          values
-        );
+          const values = {
+            RFC: XML['cfdi:Comprobante']['cfdi:Receptor']['Rfc'],
+            idCatFormaPago:
+              XML['cfdi:Comprobante']['MetodoPago'] === 'PUE' ? 1 : 2,
+            serie: XML['cfdi:Comprobante']['Serie'],
+            folio: XML['cfdi:Comprobante']['Folio'],
+            nombre: XML['cfdi:Comprobante']['cfdi:Receptor']['Nombre'],
+            uuid: XML['cfdi:Comprobante']['cfdi:Complemento'][
+              'tfd:TimbreFiscalDigital'
+            ]['UUID'],
+            fecha: XML['cfdi:Comprobante']['Fecha'].replace('T', ' '),
+            version: XML['cfdi:Comprobante']['Version'],
+            relacionados: `,null,'${XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos']['pago20:Pago']['pago20:DoctoRelacionado']['IdDocumento']}'`,
+            uuidRelacionado:
+              XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
+                'pago20:Pago'
+              ]['pago20:DoctoRelacionado']['IdDocumento'],
+            claveProdServ: concepto['ClaveProdServ'],
+            descripcion: concepto['Descripcion'],
+            numParcialidad:
+              XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
+                'pago20:Pago'
+              ]['pago20:DoctoRelacionado']['NumParcialidad'],
+            impSaldoAnt:
+              XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
+                'pago20:Pago'
+              ]['pago20:DoctoRelacionado']['ImpSaldoAnt'],
+            impSaldoInsoluto:
+              XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
+                'pago20:Pago'
+              ]['pago20:DoctoRelacionado']['ImpSaldoInsoluto'],
+            impPagado:
+              XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
+                'pago20:Pago'
+              ]['pago20:DoctoRelacionado']['ImpPagado'],
+            fechaPago: XML['cfdi:Comprobante']['cfdi:Complemento'][
+              'pago20:Pagos'
+            ]['pago20:Pago']['FechaPago'].replace('T', ' '),
+            fechaCreacion: XML['cfdi:Comprobante']['cfdi:Complemento'][
+              'tfd:TimbreFiscalDigital'
+            ]['FechaTimbrado'].replace('T', ' '),
+            fechaQuery: XML['cfdi:Comprobante']['cfdi:Complemento'][
+              'pago20:Pagos'
+            ]['pago20:Pago']['FechaPago'].substring(0, 10),
+          };
+
+          await renderToFolder(
+            './rep-parser/queries.sql',
+            `./queries/${folder}/`,
+            values
+          );
+        });
       });
     }
   }
