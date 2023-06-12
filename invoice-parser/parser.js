af7df630ef670e103c069e8b40b46120fadf0edc
Adrian Ayala <adrian.ayala17@gmail.com>
2023-06-12 13:01:01 -0600

    Mejora el script para parsear facturas
    Ahora el script hace toda la lectura de folders y achivos de manera síncrona para poder juntar el resultado del proceso de parsear el template en un solo archivo SQL

--- invoice-parser/parser.js
+++ invoice-parser/parser.js
@@ -1,63 +1,101 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import parser from 'xml2json';
 import templateFile from 'template-file';
-import path from 'path';
-import async from 'async';
 
-async function ls(directory) {
-  const folders = await fs.promises.readdir(directory);
+function parseXMLs() {
+  const basePath = './invoice-parser/xmls';
   const { renderFile } = templateFile;
+  const dirs = fs.readdir(basePath);
+  const string = [];
 
-  for (const folder of folders) {
-    if (folder !== '.DS_Store' && folder !== '.gitkeep') {
-      fs.readdir(`${directory}/${folder}`, async function (err, file) {
+  dirs.then((subDirs) => {
+    subDirs.shift();
+    subDirs.shift();
+
+    for (const dir of subDirs) {
+      fs.readdir(`${basePath}/${dir}`).then((files) => {
         let xmlPath;
 
-        if (file.length > 1) {
-          xmlPath = `${directory}/${folder}/${file[1]}`;
+        if (files.length > 1) {
+          xmlPath = `${basePath}/${dir}/${files[1]}`;
         } else {
-          xmlPath = `${directory}/${folder}/${file[0]}`;
+          xmlPath = `${basePath}/${dir}/${files[0]}`;
         }
 
-        fs.readFile(xmlPath, async function (err, data) {
-          const XML = JSON.parse(parser.toJson(data));
+        fs.readFile(xmlPath)
+          .then(async (content) => {
+            const XML = JSON.parse(parser.toJson(content));
 
-          const folder =
-            XML['cfdi:Comprobante']['Serie'] + XML['cfdi:Comprobante']['Folio'];
+            const values = {
+              RFC: XML['cfdi:Comprobante']['cfdi:Receptor']['Rfc'],
+              idCatFormaPago:
+                XML['cfdi:Comprobante']['MetodoPago'] === 'PUE' ? 1 : 2,
+              serie: XML['cfdi:Comprobante']['Serie'],
+              folio: XML['cfdi:Comprobante']['Folio'],
+              nombre: XML['cfdi:Comprobante']['cfdi:Receptor']['Nombre'],
+              total: XML['cfdi:Comprobante']['Total'],
+              iva: XML['cfdi:Comprobante']['cfdi:Impuestos'][
+                'TotalImpuestosTrasladados'
+              ],
+              subTotal: XML['cfdi:Comprobante']['SubTotal'],
+              uuid: XML['cfdi:Comprobante']['cfdi:Complemento'][
+                'tfd:TimbreFiscalDigital'
+              ]['UUID'],
+              fecha: XML['cfdi:Comprobante']['Fecha'].replace('T', ' '),
+              metodoPago: XML['cfdi:Comprobante']['MetodoPago'],
+              version: XML['cfdi:Comprobante']['Version'],
+              uuidRelacionado: XML['cfdi:Comprobante']['cfdi:CfdiRelacionados']
+                ? `,4,'${XML['cfdi:Comprobante']['cfdi:CfdiRelacionados']['cfdi:CfdiRelacionado']['UUID']}'`
+                : '',
+              conceptos: [],
+            };
 
-          const values = {
-            RFC: XML['cfdi:Comprobante']['cfdi:Receptor']['Rfc'],
-            idCatFormaPago:
-              XML['cfdi:Comprobante']['MetodoPago'] === 'PUE' ? 1 : 2,
-            serie: XML['cfdi:Comprobante']['Serie'],
-            folio: XML['cfdi:Comprobante']['Folio'],
-            nombre: XML['cfdi:Comprobante']['cfdi:Receptor']['Nombre'],
-            total: XML['cfdi:Comprobante']['Total'],
-            iva: XML['cfdi:Comprobante']['cfdi:Impuestos'][
-              'TotalImpuestosTrasladados'
-            ],
-            subTotal: XML['cfdi:Comprobante']['SubTotal'],
-            uuid: XML['cfdi:Comprobante']['cfdi:Complemento'][
-              'tfd:TimbreFiscalDigital'
-            ]['UUID'],
-            fecha: XML['cfdi:Comprobante']['Fecha'].replace('T', ' '),
-            metodoPago: XML['cfdi:Comprobante']['MetodoPago'],
-            version: XML['cfdi:Comprobante']['Version'],
-            uuidRelacionado: XML['cfdi:Comprobante']['cfdi:CfdiRelacionados']
-              ? `,4,'${XML['cfdi:Comprobante']['cfdi:CfdiRelacionados']['cfdi:CfdiRelacionado']['UUID']}'`
-              : '',
-            conceptos: [],
-          };
+            if (values.uuidRelacionado !== '')
+              values.columnasUuidRelacionado =
+                ', satTipoRelacion, uuidsRelacionados';
 
-          if (values.uuidRelacionado !== '')
-            values.columnasUuidRelacionado =
-              ', satTipoRelacion, uuidsRelacionados';
+            const conceptos =
+              XML['cfdi:Comprobante']['cfdi:Conceptos']['cfdi:Concepto'];
 
-          const conceptos =
-            XML['cfdi:Comprobante']['cfdi:Conceptos']['cfdi:Concepto'];
+            if (Array.isArray(conceptos)) {
+              conceptos.forEach(async (concepto) => {
+                const conceptValues = {
+                  serie: values.serie,
+                  folio: values.folio,
+                  uuid: values.uuid,
+                  RFC: values.RFC,
+                  fecha: values.fecha,
+                  noIdentificacion: concepto['NoIdentificacion'],
+                  unidad: concepto['Unidad'],
+                  descripcion: concepto['Descripcion'],
+                  importe: concepto['Importe'],
+                  valorUnitario: concepto['ValorUnitario'],
+                  claveProdServ: concepto['ClaveProdServ'],
+                  claveUnidad: concepto['ClaveUnidad'],
+                  cantidad: concepto['Cantidad'],
+                  impuestoConcepto:
+                    concepto['cfdi:Impuestos']['cfdi:Traslados'][
+                      'cfdi:Traslado'
+                    ]['Importe'],
+                  totalConcepto:
+                    parseFloat(concepto.Importe) +
+                    parseFloat(
+                      concepto['cfdi:Impuestos']['cfdi:Traslados'][
+                        'cfdi:Traslado'
+                      ]['Importe']
+                    ),
+                  descuentoPorcentaje:
+                    concepto['Descuento'] > 0
+                      ? parseFloat(concepto.Importe) /
+                        parseFloat(concepto['Descuento'])
+                      : 0,
+                };
+                values.conceptos.push(conceptValues);
+              });
+            } else {
+              const concepto =
+                XML['cfdi:Comprobante']['cfdi:Conceptos']['cfdi:Concepto'];
 
-          if (Array.isArray(conceptos)) {
-            conceptos.forEach(async (concepto) => {
               const conceptValues = {
                 serie: values.serie,
                 folio: values.folio,
@@ -89,78 +127,23 @@ async function ls(directory) {
                       parseFloat(concepto['Descuento'])
                     : 0,
               };
+
               values.conceptos.push(conceptValues);
-            });
-          } else {
-            const concepto =
-              XML['cfdi:Comprobante']['cfdi:Conceptos']['cfdi:Concepto'];
+            }
 
-            const conceptValues = {
-              serie: values.serie,
-              folio: values.folio,
-              uuid: values.uuid,
-              RFC: values.RFC,
-              fecha: values.fecha,
-              noIdentificacion: concepto['NoIdentificacion'],
-              unidad: concepto['Unidad'],
-              descripcion: concepto['Descripcion'],
-              importe: concepto['Importe'],
-              valorUnitario: concepto['ValorUnitario'],
-              claveProdServ: concepto['ClaveProdServ'],
-              claveUnidad: concepto['ClaveUnidad'],
-              cantidad: concepto['Cantidad'],
-              impuestoConcepto:
-                concepto['cfdi:Impuestos']['cfdi:Traslados']['cfdi:Traslado'][
-                  'Importe'
-                ],
-              totalConcepto:
-                parseFloat(concepto.Importe) +
-                parseFloat(
-                  concepto['cfdi:Impuestos']['cfdi:Traslados']['cfdi:Traslado'][
-                    'Importe'
-                  ]
-                ),
-              descuentoPorcentaje:
-                concepto['Descuento'] > 0
-                  ? parseFloat(concepto.Importe) /
-                    parseFloat(concepto['Descuento'])
-                  : 0,
-            };
+            const result = await renderFile(
+              './invoice-parser/template.txt',
+              values
+            );
 
-            values.conceptos.push(conceptValues);
-          }
-
-          const SQL = await renderFile('./invoice-parser/template.sql', values);
-
-          fs.writeFile(`./SQL/${values.RFC}.sql`, SQL, (err) => {
-            if (err) console.log(err);
+            string.push(result);
+          })
+          .then(() => {
+            fs.writeFile('./SQL/queries.sql', string.join('\n\n'));
           });
-        });
       });
     }
-  }
-
-  return new Promise((resolve, reject) => {
-    fs.readdir('./SQL', (err, files) => {
-      if (err) return reject(err);
-
-      files = files.map((file) => path.join('./SQL', file));
-
-      // Quita el .gitkeep y el .DS_Store
-      files.shift();
-      files.shift();
-
-      async.map(files, fs.readFile, (err, results) => {
-        if (err) return reject(err);
-
-        fs.writeFile('./SQL/queries.sql', results.join('\n'), (err) => {
-          if (err) return reject(err);
-
-          resolve();
-        });
-      });
-    });
   });
 }
 
-ls('./invoice-parser/xmls').catch(console.error);
+parseXMLs();
