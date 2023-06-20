aa1fc4038b200128c1592daca01168db472df605
Adrian Ayala <adrian.ayala17@gmail.com>
2023-06-19 18:53:46 -0600

    Agrega validaciones para REPs de doble pago

--- rep-parser/parser.js
+++ rep-parser/parser.js
@@ -42,39 +42,57 @@ function parseXMLs() {
               ]['UUID'],
               fecha: XML['cfdi:Comprobante']['Fecha'].replace('T', ' '),
               version: XML['cfdi:Comprobante']['Version'],
-              relacionados: `,null,'${XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos']['pago20:Pago']['pago20:DoctoRelacionado']['IdDocumento']}'`,
-              uuidRelacionado:
-                XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
-                  'pago20:Pago'
-                ]['pago20:DoctoRelacionado']['IdDocumento'],
-              claveProdServ: concepto['ClaveProdServ'],
-              descripcion: concepto['Descripcion'],
-              numParcialidad:
-                XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
-                  'pago20:Pago'
-                ]['pago20:DoctoRelacionado']['NumParcialidad'],
-              impSaldoAnt:
-                XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
-                  'pago20:Pago'
-                ]['pago20:DoctoRelacionado']['ImpSaldoAnt'],
-              impSaldoInsoluto:
-                XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
-                  'pago20:Pago'
-                ]['pago20:DoctoRelacionado']['ImpSaldoInsoluto'],
-              impPagado:
-                XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
-                  'pago20:Pago'
-                ]['pago20:DoctoRelacionado']['ImpPagado'],
-              fechaPago: XML['cfdi:Comprobante']['cfdi:Complemento'][
-                'pago20:Pagos'
-              ]['pago20:Pago']['FechaPago'].replace('T', ' '),
               fechaCreacion: XML['cfdi:Comprobante']['cfdi:Complemento'][
                 'tfd:TimbreFiscalDigital'
               ]['FechaTimbrado'].replace('T', ' '),
-              fechaQuery: XML['cfdi:Comprobante']['cfdi:Complemento'][
-                'pago20:Pagos'
-              ]['pago20:Pago']['FechaPago'].substring(0, 10),
+              claveProdServ: concepto['ClaveProdServ'],
+              descripcion: concepto['Descripcion'],
             };
+
+            const pagos =
+              XML['cfdi:Comprobante']['cfdi:Complemento']['pago20:Pagos'][
+                'pago20:Pago'
+              ];
+
+            if (pagos.length > 1) {
+              let relacionados = '';
+
+              values.uuidRelacionado =
+                pagos[0]['pago20:DoctoRelacionado']['IdDocumento'];
+              values.numParcialidad =
+                pagos[0]['pago20:DoctoRelacionado']['NumParcialidad'];
+
+              values.impSaldoAnt = 0;
+              values.impSaldoInsoluto = 0;
+              values.impPagado = 0;
+
+              values.fechaPago = pagos[0]['FechaPago'].replace('T', ' ');
+              values.fechaQuery = pagos[0]['FechaPago'].substring(0, 10);
+
+              pagos.forEach((pago) => {
+                const data = pago['pago20:DoctoRelacionado'];
+
+                relacionados = `,${data['IdDocumento']}` + relacionados;
+                values.impSaldoAnt += parseFloat(data['ImpSaldoAnt']);
+                values.impSaldoInsoluto += parseFloat(data['ImpSaldoInsoluto']);
+                values.impPagado += parseFloat(data['ImpPagado']);
+              });
+
+              values.relacionados = `,null, '${relacionados.substring(1)}'`;
+            } else {
+              values.relacionados = `,null,'${pagos['pago20:DoctoRelacionado']['IdDocumento']}'`;
+              values.uuidRelacionado =
+                pagos['pago20:DoctoRelacionado']['IdDocumento'];
+              values.numParcialidad =
+                pagos['pago20:DoctoRelacionado']['NumParcialidad'];
+              values.impSaldoAnt =
+                pagos['pago20:DoctoRelacionado']['ImpSaldoAnt'];
+              values.impSaldoInsoluto =
+                pagos['pago20:DoctoRelacionado']['ImpSaldoInsoluto'];
+              values.impPagado = pagos['pago20:DoctoRelacionado']['ImpPagado'];
+              values.fechaPago = pagos['FechaPago'].replace('T', ' ');
+              values.fechaQuery = pagos['FechaPago'].substring(0, 10);
+            }
 
             const result = await renderFile(
               './rep-parser/template.txt',
